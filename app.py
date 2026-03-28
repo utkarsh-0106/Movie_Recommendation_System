b723a3cd9060ba4bd48156f9209baabb45523886
Utkarsh Maheshwari <99492433+utkarsh-0106@users.noreply.github.com>
2026-03-28 22:06:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def recommend_movies():
 
 # For local development only
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
